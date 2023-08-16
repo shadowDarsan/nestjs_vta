5a945a01c5cce5f9304e5f3b30f6f1413eb3cd2e
ShadowDarsan <panerusamrat@gmail.com>
2023-08-16 20:40:11 +0545

    removed unused imports

--- src/user-details/dto/create-userDetails.dto.ts
+++ src/user-details/dto/create-userDetails.dto.ts
@@ -1,4 +1,4 @@
-import { IsDate, IsDateString, IsNotEmpty, IsString } from 'class-validator'
+import { IsDateString, IsNotEmpty, IsString } from 'class-validator'
 
 export class CreateUserDetailsDto {
 	@IsString()
